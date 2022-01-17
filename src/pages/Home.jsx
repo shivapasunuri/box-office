be08aea56ba985b50d1520d1f9892ffee4424336
shivapasunuri <shivapasunuri005@gmail.com>
2022-01-17 23:03:24 +0530

    Displayed show results

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,16 @@
 import React, { useState } from "react";
 import MainPageLayout from "../components/MainPageLayout";
+import { apiGet } from "../misc/config";
 
 const Home = () => {
   const [input, setInput] = useState("");
+  const [results, setResults] = useState(null);
 
   const onSearch = () => {
-    // https://api.tvmaze.com/search/shows?q=lucifer
-    fetch(`https://api.tvmaze.com/search/shows?q=${input}`).then((resp) =>
-      resp.json().then((result) => {
-        console.log(result);
-      })
-    );
+    apiGet(`/search/shows?q=${input}`).then((result) => {
+      setResults(result);
+      console.log(result);
+    });
   };
 
   const onInputChange = (event) => {
@@ -22,6 +22,21 @@ const Home = () => {
       onSearch();
     }
   };
+
+  const renderResults = () => {
+    if (results && results.length === 0) {
+      return <div>No Results</div>;
+    } else if (results && results.length > 0) {
+      return (
+        <div>
+          {results.map((item) => (
+            <div key={item.show.id}>{item.show.name}</div>
+          ))}
+        </div>
+      );
+    }
+  };
+
   return (
     <MainPageLayout>
       <input
@@ -33,6 +48,7 @@ const Home = () => {
       <button type="button" onClick={onSearch}>
         Search
       </button>
+      {renderResults()}
     </MainPageLayout>
   );
 };

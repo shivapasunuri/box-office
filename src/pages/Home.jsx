9a651dbeeeef2709a7a538907e318c2f8e8d2cd2
shivapasunuri <shivapasunuri005@gmail.com>
2022-01-17 22:17:50 +0530

    Added input search bar and fetched the results

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import MainPageLayout from "../components/MainPageLayout";
 
 const Home = () => {
-  return <MainPageLayout> This is home </MainPageLayout>;
+  const [input, setInput] = useState("");
+
+  const onSearch = () => {
+    // https://api.tvmaze.com/search/shows?q=lucifer
+    fetch(`https://api.tvmaze.com/search/shows?q=${input}`).then((resp) =>
+      resp.json().then((result) => {
+        console.log(result);
+      })
+    );
+  };
+
+  const onInputChange = (event) => {
+    setInput(event.target.value);
+  };
+
+  const onKeyDown = (e) => {
+    if (e.keyCode === 13) {
+      onSearch();
+    }
+  };
+  return (
+    <MainPageLayout>
+      <input
+        type="text"
+        onChange={onInputChange}
+        onKeyDown={onKeyDown}
+        value={input}
+      />
+      <button type="button" onClick={onSearch}>
+        Search
+      </button>
+    </MainPageLayout>
+  );
 };
 
 export default Home;

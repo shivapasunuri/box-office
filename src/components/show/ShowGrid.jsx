5fbd4b4dfcc3df2a80337bf7ccb320bd3c64f629
shivapasunuri <shivapasunuri005@gmail.com>
2022-01-20 00:11:33 +0530

    Added show page

--- src/components/show/ShowGrid.jsx
+++ src/components/show/ShowGrid.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ShowCard from "./ShowCard";
 import IMG_NOT_FOUND from "../../images/not-found.png";
 import { FlexGrid } from "../styled";
+
 const ShowGrid = ({ data }) => {
   return (
     <FlexGrid>
---
+++ src/pages/Show.jsx
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { apiGet } from "../misc/config";
+
+const Show = () => {
+  const { id } = useParams();
+  const [show, setShow] = useState(null);
+  useEffect(() => {
+    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then((results) => {
+      setShow(results);
+    });
+  }, [id]);
+  console.log("show: ", show);
+
+  return <h1>This is show pages</h1>;
+};
+
+export default Show;

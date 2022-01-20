7a6eab26d4cae8deef2e7042c94bae0ba0d24104
shivapasunuri <shivapasunuri005@gmail.com>
2022-01-20 18:17:53 +0530

    Using custom hooks for starred shows

--- src/misc/customHooks.js
+++ src/misc/customHooks.js
@@ -1,13 +1,15 @@
-import { useEffect, useReducer } from "react";
+import { useReducer, useEffect } from "react";
 
 function showsReducer(prevState, action) {
   switch (action.type) {
     case "ADD": {
       return [...prevState, action.showId];
     }
+
     case "REMOVE": {
       return prevState.filter((showId) => showId !== action.showId);
     }
+
     default:
       return prevState;
   }
@@ -16,11 +18,14 @@ function showsReducer(prevState, action) {
 function usePersistedReducer(reducer, initialState, key) {
   const [state, dispatch] = useReducer(reducer, initialState, (initial) => {
     const persisted = localStorage.getItem(key);
+
     return persisted ? JSON.parse(persisted) : initial;
   });
+
   useEffect(() => {
     localStorage.setItem(key, JSON.stringify(state));
   }, [state, key]);
+
   return [state, dispatch];
 }
 
